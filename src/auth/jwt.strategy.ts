64ed926631d405f5b3e37d7a3159eac41e553923
ayorich <kayodele17@yahoo.com>
2021-02-03 07:26:14 +0100

    config-added

--- src/auth/jwt.strategy.ts
+++ src/auth/jwt.strategy.ts
@@ -6,16 +6,16 @@ import { Strategy, ExtractJwt } from 'passport-jwt';
 import { JwtPayload } from './jwt-payload.interface';
 import { User } from './user.entity';
 import { UserRepository } from './user.repository';
-
+import * as config from 'config';
 @Injectable()
 export class JwtStrategy extends PassportStrategy(Strategy) {
   constructor(
-    // @InjectRepository(UserRepository)
+    @InjectRepository(UserRepository)
     private userRepository: UserRepository,
   ) {
     super({
       jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
-      secretOrKey: 'topsecret51',
+      secretOrKey: process.env.JWT_SECRET || config.get('jwt.secret'),
     });
   }
   async validate(payload: JwtPayload): Promise<User> {
